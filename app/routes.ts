0038c3cf3cabe12a6ca4a5a6cca23beb83306de2
Marc Eley <marc@eley.dk>
2025-09-18 23:27:32 +0200

    .

--- app/routes.ts
+++ app/routes.ts
@@ -7,7 +7,6 @@ export default [
   route("api/match-production", "routes/api.match-production.ts"),
   route("api/products", "routes/api.products.ts"),
   route("api/extract-recipe", "routes/api.extract-recipe.ts"),
-  route("api/tokens", "routes/api.tokens.ts"),
   route("products", "routes/products.tsx"),
   route("match", "routes/match.tsx"),
   route("test-production", "routes/test-production.tsx"),
